cf0476d793c30dc52ae38385ee5dee7a1e5d57c2
zaidansari-sudo <zaidasari0515@gmail.com>
2025-12-26 16:02:15 +0530

    rate limiter integration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const connectDB = require("./lib/connectDB");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -9,13 +10,27 @@ const cartRoutes = require("./routes/cartRoute");
 const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/upload.route");
 
+
 dotenv.config();
 
 const app = express();
+// 🔐 Global Rate Limiter (applies to all APIs)
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // 100 requests per IP
+  message: {
+    error: "Too many requests. Please try again later."
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(globalLimiter);
 
 app.use("/api/products", productRoutes);
 app.use("/api/auth",userRoutes);
